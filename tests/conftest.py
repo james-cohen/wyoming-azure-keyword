696deced559930f58946b788ee121a80bd2adce9
James Cohen <github@jamescohen.com>
2025-10-08 22:19:16 +0100

    fix test init error

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,16 +5,13 @@ import os
 import pytest
 from dotenv import load_dotenv
 
-from wyoming_azure_keyword import KeywordDetectionConfig
-
 load_dotenv()
 
 
 @pytest.fixture
 def azure_speech_keyword_args():
     """Return AzureSpeechKeyword instance."""
-    args = KeywordDetectionConfig(
-        model_path=os.environ.get("MODEL_PATH") or "",
-        keyword_name=os.environ.get("KEYWORD_NAME") or "",
-    )
-    return args
+    return {
+        "model_path": os.environ.get("MODEL_PATH") or "",
+        "keyword_name": os.environ.get("KEYWORD_NAME") or "",
+    }
